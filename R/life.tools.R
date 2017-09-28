618345e49bfe59ea56f6f48f07af52d4dd74c071
Thomas Peschel <tpeschel@life.uni-leipzig.de>
2017-09-28 12:14:44 +0200

    add table.columns

--- R/life.tools.R
+++ R/life.tools.R
@@ -103,3 +103,21 @@ list.append <-
 					name
 
 		list }
+
+#' TABLE COLUMNS
+#'
+#' @param data dataframe which columns should be summarised
+#' @param show.missings flag for summing missings or availables
+#'
+#' @return print of missings or availables, depending on show.missings
+#' @export
+#'
+#' @examples
+#' table.columns(data.frame(x=c(1:3),y=c(NA,1,NA),z=c(NA,NA,NA)))
+#' table.columns(data.frame(x=c(1:3),y=c(NA,1,NA),z=c(NA,NA,NA)),F)
+table.columns <-
+	function( data, show.missings = T ) {
+		if( show.missings )
+			return( sapply( data, function( d ) sum( is.na( d ) ) ) )
+		else
+			return( sapply( data, function( d ) sum( !is.na( d ) ) ) ) }
