345d6e41165a8a3665c3c102ff519172ce1ea1f1
Thomas Peschel <tpeschel@life.uni-leipzig.de>
2017-10-18 23:41:44 +0200

    move adjust and load.pkgs into life.tools

--- R/life.tools.R
+++ R/life.tools.R
@@ -2,6 +2,136 @@
 # Sys.timezone( )
 Sys.setenv( TZ = "Europe/Berlin" )
 
+#' LOAD PACKAGES
+#' @name load.pkgs
+#'
+#' @description load packages tries to load a package into the environment. If it fails, it tries to install the package and loads it afterwards.
+#' @param pkgs
+#'
+#' @export
+#'
+#' @examples
+#' load.pkgs( c( "dplyr", "ggplot2", "ggthemes", "reshape2" ) )
+load.pkgs <-
+	function( pkgs =c( "dplyr", "ggplot2", "ggthemes", "reshape2" )) {
+
+		exist <-
+			pkgs %in% rownames( installed.packages( ) )
+
+		if( any( !exist ) )
+			install.packages( pkgs[ !exist ] )
+
+		sapply( pkgs, library, character.only = TRUE ) }
+
+#' ADJUST LINEARLY
+#'
+#' @description Adjust a dependent variable y linearly to several dependent variables x1, x2 ...
+#' @param formula A formula that gives the relationship of the adjustment.
+#' @param data A dataframe which contains the dependent and the independent variables.
+#'
+#' @return A dataframe containing a new column with the adjusted values.
+#' @export
+#'
+#' @examples
+#' hlpr4life::load.pkgs( c( "hlpr4life", "ggplot2" ) )
+#' set.seed( 1 )
+#' num <- 100
+#' sexes <- c( "female", "male" )
+#' d <- data.frame( sex = sample( sexes, num, T, prob = c( .3, .7 )  ), age = round( runif( num, 0, 20 ), 1 ) )
+#' d$y <- c( 30, 35 )[ match( d$sex, sexes ) ] + c( 2, 3 )[ match( d$sex, sexes ) ] * d$age + rnorm( num, 0, 10 )
+#' d <- adjust.linearly( y ~ age, d )
+#' d <- adjust.linearly( y ~ sex, d )
+#' d <- adjust.linearly( y ~ age * sex, d )
+#' thm          <- list( theme_bw( ), geom_point( ), geom_smooth( method = "lm" ) )
+#' thm.brown    <- list( theme_bw( ), geom_point( col = "green3" ), geom_smooth( col = "green3", method = "lm" ) )
+#' thm.bicol    <- list.append( thm, scale_color_manual( values = c( "firebrick1", "steelblue3" ), guide = F ) )
+#' thm.bicolfac <- list.append( thm.bicol, facet_grid( sex ~ . ) )
+#' ggsubplot(
+#'   ggplot( d, aes( age, y ) ) + thm.brown,
+#'   ggplot( d, aes( age, y, col = sex ) ) + thm.bicol,
+#'   ggplot( d, aes( age, y.adj.for.age ) ) + thm.brown,
+#'   ggplot( d, aes( age, y.adj.for.age, col = sex ) ) + thm.bicol,
+#'   ggplot( d, aes( age, y.adj.for.sex ) ) + thm.brown,
+#'   ggplot( d, aes( age, y.adj.for.sex, col = sex ) ) + thm.bicol,
+#'   ggplot( d, aes( age, y.adj.for.age.sex, col = sex ) ) + thm.bicol,
+#'   ggplot( d, aes( age, y.adj.for.age.sex, col = sex ) ) + thm.bicolfac,
+#'   cols = 4 )
+adjust.linearly <-
+	function( formula, data ) {
+
+		a.v <-
+			all.vars( formula )
+
+		nm <-
+			paste0( a.v[ 1 ], ".adj.for" )
+
+		for( a in a.v[ -1 ] )
+
+			nm <-
+			paste0( nm, ".", a )
+
+		data[ , nm ] <-
+			( lm( formula, data ) )$residuals + mean( unlist( data[ , a.v[ 1 ] ] ), na.rm = T )
+
+		data }
+########################################################################
+#' ADJUST LINEARLY and STANDARIZE
+#'
+#' @description Adjust a dependent variable y linearly to several dependent variables x1, x2 ...
+#' and standardize afterwards the adjusted values.
+#' Requirements to the data: Homoscedasticity and Linearity of y ~ x...
+#' @param formula A formula that gives the relationship of the adjustment.
+#' @param data A dataframe which contains the dependent and the independent variables.
+#'
+#' @return A dataframe containing a new column with the adjusted and standardized values.
+#' @export
+#'
+#' @examples
+#' hlpr4life::load.pkgs( c( "hlpr4life", "ggplot2" ) )
+#' set.seed( 1 )
+#' num <- 100
+#' sexes <- c( "female", "male" )
+#' d <- data.frame( sex = sample( sexes, num, T, prob = c( .3, .7 )  ), age = round( runif( num, 0, 20 ), 1 ) )
+#' d$y <- c( 30, 35 )[ match( d$sex, sexes ) ] + c( 2, 3 )[ match( d$sex, sexes ) ] * d$age + rnorm( num, 0, 10 )
+#' d <- adjust.linearly.std( y ~ age, d )
+#' d <- adjust.linearly.std( y ~ sex, d )
+#' d <- adjust.linearly.std( y ~ age * sex, d )
+#' thm          <- list( theme_bw( ), geom_point( ), geom_smooth( method = "lm" ) )
+#' thm.brown    <- list( theme_bw( ), geom_point( col = "green3" ), geom_smooth( col = "green3", method = "lm" ) )
+#' thm.bicol    <- list.append( thm, scale_color_manual( values = c( "firebrick1", "steelblue3" ), guide = F ) )
+#' thm.bicolfac <- list.append( thm.bicol, facet_grid( sex ~ . ) )
+#' ggsubplot(
+#'   ggplot( d, aes( age, y ) ) + thm.brown,
+#'   ggplot( d, aes( age, y, col = sex ) ) + thm.bicol,
+#'   ggplot( d, aes( age, y.std.for.age ) ) + thm.brown,
+#'   ggplot( d, aes( age, y.std.for.age, col = sex ) ) + thm.bicol,
+#'   ggplot( d, aes( age, y.std.for.sex ) ) + thm.brown,
+#'   ggplot( d, aes( age, y.std.for.sex, col = sex ) ) + thm.bicol,
+#'   ggplot( d, aes( age, y.std.for.age.sex, col = sex ) ) + thm.bicol,
+#'   ggplot( d, aes( age, y.std.for.age.sex, col = sex ) ) + thm.bicolfac,
+#'   cols = 4 )
+adjust.linearly.std <-
+	function( formula, data ) {
+
+		a.v <-
+			all.vars( formula )
+
+		nm <-
+			paste0( a.v[ 1 ], ".std.for" )
+
+		for( a in a.v[ -1 ] )
+
+			nm <-
+			paste0( nm, ".", a )
+
+		data[ , nm ] <-
+			( lm( formula, data ) )$residuals
+
+		data[ , nm ] <-
+			data[ , nm ] / sd( unlist( data[ , nm ] ), na.rm = T )
+
+		data }
+
 #' CALENDAR
 #'
 #' @description creates a simple calendar from first date to last date
@@ -141,14 +271,22 @@ list.append <-
 #' @param data dataframe which columns should be summarised
 #' @param summary logical: if is true additionally min, median, mean and max will be tabled
 #' @param as.data.frame logical: result is shown as data frame
+#' @param horizontal.of.plot logical: result is shown in horizontal or vertical style
 #'
 #' @return table of data containing sum of missing and available data their mins and maxs.
 #' @export
 #'
 #' @examples
-#' table.df(data.frame(x=c(1:3),y=c(NA,1,NA),z=c(NA,NA,NA)))
+#' table.df(data.frame(x=c(1:3),y=c(NA,1,NA),z=c(NA,NA,NA)),F,F,F)
+#' table.df(data.frame(x=c(1:3),y=c(NA,1,NA),z=c(NA,NA,NA)),T,F,F)
+#' table.df(data.frame(x=c(1:3),y=c(NA,1,NA),z=c(NA,NA,NA)),F,T,F)
+#' table.df(data.frame(x=c(1:3),y=c(NA,1,NA),z=c(NA,NA,NA)),T,T,F)
+#' table.df(data.frame(x=c(1:3),y=c(NA,1,NA),z=c(NA,NA,NA)),F,F,T)
+#' table.df(data.frame(x=c(1:3),y=c(NA,1,NA),z=c(NA,NA,NA)),T,F,T)
+#' table.df(data.frame(x=c(1:3),y=c(NA,1,NA),z=c(NA,NA,NA)),F,T,T)
+#' table.df(data.frame(x=c(1:3),y=c(NA,1,NA),z=c(NA,NA,NA)),T,T,T)
 table.df <-
-	function( data, summary = F, as.dataframe = !summary ) {
+	function( data, summary = F, as.dataframe = !summary, horizontal.of.plot = T ) {
 
 		options( warn = -1 )
 
@@ -188,13 +326,19 @@ table.df <-
 					MEAN,
 					MAX ) }
 
+		options( warn = 0 )
+
 		if( as.dataframe ) {
 
 			return( as.data.frame( d ) ) }
 
-		options( warn = 0 )
+		if( horizontal )
+			d
 
-		d
+		if( as.dataframe )
+			as.data.frame( t( d ) )
+
+		t( d )
 	}
 
 #' REMOVE COLUMNS
