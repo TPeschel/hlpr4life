8a0dbea4e9c3e65e84598a1ea7db955f7ec65ca7
Thomas Peschel <tpeschel@life.uni-leipzig.de>
2017-10-18 14:51:04 +0200

    ml

--- todo/merge.likely.R
+++ todo/merge.likely.R
@@ -1,15 +1,15 @@
-#' merge likely
+#' MERGE APPROXIMATELY
 #'
-#' @description merge likely merges two date frames by exact matching also by nearly matching. If the difference of two variable A of dataframe 1 and variable B of dataframe 2 may lay in an interval, one has the opportunity to give the limits of the interval by min and max.
+#' @description merge.approx merges two date frames by exact matching also by nearly matching. If the difference of two variable A of dataframe 1 and variable B of dataframe 2 may lay in an interval, one has the opportunity to give the limits of the interval by min and max.
 #'
 #' @param d1 left hand side dataframe
 #' @param d2 right hand side dataframe
 #' @param by vector of column names for exact merging via equal column names
 #' @param by.x vector of column names for the left hand side dataframe for exact merging via different column names
 #' @param by.y vector of column names for the right hand side dataframe for exact merging via different column names
-#' @param by.lk vector of column names for likely merging via equal column names
-#' @param by.lk.x vector of column names for the left hand side dataframe for likely merging via different column names
-#' @param by.lk.y vector of column names for the right hand side dataframe for likely merging via different column names
+#' @param by.approx vector of column names for likely merging via equal column names
+#' @param by.approx.x vector of column names for the left hand side dataframe for likely merging via different column names
+#' @param by.approx.y vector of column names for the right hand side dataframe for likely merging via different column names
 #' @param min vector of minimum limits for likely merging
 #' @param max vector of maximum limits for likely merging
 #' @param all merge and add remaining columns from both dataframes, default is FALSE
@@ -20,29 +20,29 @@
 #' @param add.diffs if add.diffs is TRUE differences of likely merged columns are added to resulting dataframe, default is FALSE
 #'
 #' @return merged data frame
-#' @export merge.likely
+#' @export merge.approx
 #'
 #' @examples
-#' d1<-data.frame(w=sample(1:10,20,T),x=sample(1:10,20,T),y=sample(1:10,20,T),z=sample(c(1:10),20,T),id=1)
-#' d2<-data.frame(w=sample(1:10,20,T),x=sample(1:10,20,T),Y=sample(1:10,20,T),C=sample(c(1:10),20,T),id=2)
-#' merge.likely(d1=d1,d2=d2,by=c("x"),by.lk=c("w","id"),by.lk.x=c("y"),by.lk.y=c("Y"),min=c(-2,-2,-2),max=c(1,1,1),reorder.names=F,trim=F,add.diffs=F)
-#' merge.likely(d1=d1,d2=d2,by=c("x"),by.lk=c("w","id"),by.lk.x=c("y"),by.lk.y=c("Y"),min=c(-2,-2,-2),max=c(1,1,1),reorder.names=T,trim=F,add.diffs=F)
-#' merge.likely(d1=d1,d2=d2,by=c("x"),by.lk=c("w","id"),by.lk.x=c("y"),by.lk.y=c("Y"),min=c(-2,-2,-2),max=c(1,1,1),reorder.names=F,trim=T,add.diffs=F)
-#' merge.likely(d1=d1,d2=d2,by=c("x"),by.lk=c("w","id"),by.lk.x=c("y"),by.lk.y=c("Y"),min=c(-2,-2,-2),max=c(1,1,1),reorder.names=T,trim=T,add.diffs=F)
-#' merge.likely(d1=d1,d2=d2,by=c("x"),by.lk=c("w","id"),by.lk.x=c("y"),by.lk.y=c("Y"),min=c(-2,-2,-2),max=c(1,1,1),reorder.names=F,trim=F,add.diffs=T)
-#' merge.likely(d1=d1,d2=d2,by=c("x"),by.lk=c("w","id"),by.lk.x=c("y"),by.lk.y=c("Y"),min=c(-2,-2,-2),max=c(1,1,1),reorder.names=T,trim=F,add.diffs=T)
-#' merge.likely(d1=d1,d2=d2,by=c("x"),by.lk=c("w","id"),by.lk.x=c("y"),by.lk.y=c("Y"),min=c(-2,-2,-2),max=c(1,1,1),reorder.names=F,trim=T,add.diffs=T)
-#' merge.likely(d1=d1,d2=d2,by=c("x"),by.lk=c("w","id"),by.lk.x=c("y"),by.lk.y=c("Y"),min=c(-2,-2,-2),max=c(1,1,1),reorder.names=T,trim=T,add.diffs=T)
-merge.likely <-
+#' (d1<-data.frame(w=sample(1:10,20,T),x=sample(1:10,20,T),y=sample(1:10,20,T),z=sample(c(1:10),20,T),id=1))
+#' (d2<-data.frame(w=sample(1:10,20,T),x=sample(1:10,20,T),Y=sample(1:10,20,T),C=sample(c(1:10),20,T),id=2))
+#' merge.approx(d1=d1,d2=d2,by=c("x"),by.approx=c("w","id"),by.approx.x=c("y"),by.approx.y=c("Y"),min=c(-2,-2,-2),max=c(1,1,1),reorder.names=F,trim=F,add.diffs=F)
+#' merge.approx(d1=d1,d2=d2,by=c("x"),by.approx=c("w","id"),by.approx.x=c("y"),by.approx.y=c("Y"),min=c(-2,-2,-2),max=c(1,1,1),reorder.names=T,trim=F,add.diffs=F)
+#' merge.approx(d1=d1,d2=d2,by=c("x"),by.approx=c("w","id"),by.approx.x=c("y"),by.approx.y=c("Y"),min=c(-2,-2,-2),max=c(1,1,1),reorder.names=F,trim=T,add.diffs=F)
+#' merge.approx(d1=d1,d2=d2,by=c("x"),by.approx=c("w","id"),by.approx.x=c("y"),by.approx.y=c("Y"),min=c(-2,-2,-2),max=c(1,1,1),reorder.names=T,trim=T,add.diffs=F)
+#' merge.approx(d1=d1,d2=d2,by=c("x"),by.approx=c("w","id"),by.approx.x=c("y"),by.approx.y=c("Y"),min=c(-2,-2,-2),max=c(1,1,1),reorder.names=F,trim=F,add.diffs=T)
+#' merge.approx(d1=d1,d2=d2,by=c("x"),by.approx=c("w","id"),by.approx.x=c("y"),by.approx.y=c("Y"),min=c(-2,-2,-2),max=c(1,1,1),reorder.names=T,trim=F,add.diffs=T)
+#' merge.approx(d1=d1,d2=d2,by=c("x"),by.approx=c("w","id"),by.approx.x=c("y"),by.approx.y=c("Y"),min=c(-2,-2,-2),max=c(1,1,1),reorder.names=F,trim=T,add.diffs=T)
+#' merge.approx(d1=d1,d2=d2,by=c("x"),by.approx=c("w","id"),by.approx.x=c("y"),by.approx.y=c("Y"),min=c(-2,-2,-2),max=c(1,1,1),reorder.names=T,trim=T,add.diffs=T)
+merge.approx <-
     function(
-        d1,
-        d2,
+        x,
+        y,
         by = NULL,
         by.x = NULL,
         by.y = NULL,
-        by.lk = NULL,
-        by.lk.x = NULL,
-        by.lk.y = NULL,
+        by.approx = NULL,
+        by.x.approx = NULL,
+        by.y.approx = NULL,
         min = NULL,
         max = NULL,
         all = F,
@@ -53,62 +53,81 @@ merge.likely <-
         add.diffs = F ) {
 
         by.x.tot <-
-            c( by, by.x )
+            union( by, setdiff( by.x, by ) )
 
         by.y.tot <-
-            c( by, by.y )
+        	union( by, setdiff( by.y, by ) )
 
-        by.lk.x.tot <-
-            c( by.lk, by.lk.x )
+        by.approx.x.tot <-
+            union( by.approx, setdiff( by.x.approx, by.approx ) )
 
-        by.lk.y.tot <-
-            c( by.lk, by.lk.y )
+        by.approx.y.tot <-
+        	union( by.approx, setdiff( by.y.approx, by.approx ) )
 
-        by.lk.xy.equal.names <-
+        by.approx.xy.equal.names <-
             intersect(
-                by.lk.x.tot,
-                by.lk.y.tot )
+                by.approx.x.tot,
+                by.approx.y.tot )
 
-        if( 0 < length( by.lk.xy.equal.names ) ) {
+        if( 0 < length( by.approx.xy.equal.names ) ) {
 
-            by.lk.x.eql.nms <-
+            by.approx.x.eql.nms <-
                 paste0(
-                    by.lk.xy.equal.names,
+                    by.approx.xy.equal.names,
                     ".x" )
 
-            by.lk.y.eql.nms <-
+            by.approx.y.eql.nms <-
                 paste0(
-                    by.lk.xy.equal.names,
+                    by.approx.xy.equal.names,
                     ".y" )
 
-            by.lk.x.tot. <-
+            by.approx.x.tot. <-
                 c(
-                    by.lk.x.eql.nms,
-                    setdiff( by.lk.x.tot, by.lk.y.tot )	)
+                    by.approx.x.eql.nms,
+                    setdiff( by.approx.x.tot, by.approx.y.tot )	)
 
-            by.lk.y.tot <-
+            by.approx.y.tot <-
                 c(
-                    by.lk.y.eql.nms,
-                    setdiff( by.lk.y.tot, by.lk.x.tot )	)
+                    by.approx.y.eql.nms,
+                    setdiff( by.approx.y.tot, by.approx.x.tot )	)
 
-            by.lk.x.tot <-
-                by.lk.x.tot. }
+            by.approx.x.tot <-
+                by.approx.x.tot. }
 
         d <-
             merge(
                 d1,
                 d2,
                 by.x = by.x.tot,
-                by.y = by.y.tot,
-                all.x = all.x,
-                all.y = all.y )
+                by.y = by.y.tot )
 
-        for( i in seq_along( by.lk.x.tot ) ) {
+        if( all == T ) {
+        	all.x <- T
+        	all.y <- T
+        }
 
-            if( lubridate::is.POSIXct( d[ 1, by.lk.x.tot[ i ] ] ) ) {
+        if( all.x == T ) {
+        	d.allx <-
+        		dplyr::anti_join(
+	        		d1,
+	        		d2,
+	        		by.x = by.x.tot,
+	        		by.y = by.y.tot ) }
+
+        if( all.y == T ) {
+        	d.ally <-
+        		dplyr::anti_join(
+	        		d2,
+	        		d1,
+	        		by.y = by.y.tot,
+	        		by.x = by.x.tot ) }
+
+        for( i in seq_along( by.approx.x.tot ) ) {
+
+            if( lubridate::is.POSIXct( d[ 1, by.approx.x.tot[ i ] ] ) ) {
 
                 l.1.Ihk37Z._dlk__NDu2786cmn <-
-                    difftime( d[ , by.lk.y.tot[ i ] ], d[ , by.lk.x.tot[ i ] ], units = "day" )
+                    difftime( d[ , by.approx.y.tot[ i ] ], d[ , by.approx.x.tot[ i ] ], units = "day" )
 
                 l.min.max <-
                     ( min[ i ] < l.1.Ihk37Z._dlk__NDu2786cmn & l.1.Ihk37Z._dlk__NDu2786cmn <= max[ i ] )
@@ -119,17 +138,17 @@ merge.likely <-
                         cbind( d, l.1.Ihk37Z._dlk__NDu2786cmn )
 
                     names( d )[ names( d ) == "l.1.Ihk37Z._dlk__NDu2786cmn" ] <-
-                        paste0( by.lk.y.tot[ i ], "-" , by.lk.x.tot[ i ] ) }
+                        paste0( by.approx.y.tot[ i ], "-" , by.approx.x.tot[ i ] ) }
 
                 d <-
                     d[ l.min.max, ]
 
             } else {
 
-                if( is.numeric( d[ 1, by.lk.x.tot[ i ] ] ) ) {
+                if( is.numeric( d[ 1, by.approx.x.tot[ i ] ] ) ) {
 
                     l.1.Ihk37Z._dlk__NDu2786cmn <-
-                        d[ , by.lk.y.tot[ i ] ] - d[ , by.lk.x.tot[ i ] ]
+                        d[ , by.approx.y.tot[ i ] ] - d[ , by.approx.x.tot[ i ] ]
 
                     l.min.max <-
                         ( min[ i ] < l.1.Ihk37Z._dlk__NDu2786cmn & l.1.Ihk37Z._dlk__NDu2786cmn <= max[ i ] )
@@ -139,18 +158,25 @@ merge.likely <-
                             cbind( d, l.1.Ihk37Z._dlk__NDu2786cmn )
 
                         names( d )[ names( d ) == "l.1.Ihk37Z._dlk__NDu2786cmn" ] <-
-                            paste0( by.lk.y.tot[ i ], "-" , by.lk.x.tot[ i ] ) }
+                            paste0( by.approx.y.tot[ i ], "-" , by.approx.x.tot[ i ] ) }
 
                     d <-
                         d[ l.min.max, ] } }
+        	if( all.x )
+        		d <-
+        			dplyr::bind_rows( d, d.allx )
 
-            if( trim ) {
+        	if( all.y )
+        		d <-
+        			dplyr::bind_rows( d, d.ally )
 
-                d[ , by.lk.y.tot[ i ] ] <-
+        	if( trim ) {
+
+                d[ , by.approx.y.tot[ i ] ] <-
                     NULL
 
-                names( d )[ names( d ) == by.lk.x.tot[ i ] ] <-
-                    sub( "(\\w*).x", "\\1", by.lk.x.tot[ i ], perl = T ) } }
+                names( d )[ names( d ) == by.approx.x.tot[ i ] ] <-
+                    sub( "(\\w*).x", "\\1", by.approx.x.tot[ i ], perl = T ) } }
 
         if( reorder.names ) {
 
@@ -159,17 +185,17 @@ merge.likely <-
                 if( add.diffs ) {
 
                     d.n <-
-                        sapply( t( cbind( cbind( by.lk.x.tot, by.lk.y.tot ), paste0( by.lk.y.tot, "-", by.lk.x.tot ) ) ), paste )
+                        sapply( t( cbind( cbind( by.approx.x.tot, by.approx.y.tot ), paste0( by.approx.y.tot, "-", by.approx.x.tot ) ) ), paste )
 
                 } else {
 
-                    by.lk.tot <-
-                        cbind( by.lk.x.tot, by.lk.y.tot )
+                    by.approx.tot <-
+                        cbind( by.approx.x.tot, by.approx.y.tot )
 
-                    if( !is.null( by.lk.tot ) ) {
+                    if( !is.null( by.approx.tot ) ) {
 
                         d.n <-
-                            t( by.lk.tot ) }
+                            t( by.approx.tot ) }
                     else {
                         d.n <-
                             NULL }
@@ -182,11 +208,11 @@ merge.likely <-
                 if( add.diffs ) {
 
                     d.n <-
-                        sapply( t( cbind( sub( "(\\w*).x", "\\1", by.lk.x.tot, perl = T ), paste0( by.lk.y.tot, "-", by.lk.x.tot ) ) ), paste )
+                        sapply( t( cbind( sub( "(\\w*).x", "\\1", by.approx.x.tot, perl = T ), paste0( by.approx.y.tot, "-", by.approx.x.tot ) ) ), paste )
                 } else {
 
                     d.n <-
-                        sub( "(\\w*).x", "\\1", by.lk.x.tot, perl = T )
+                        sub( "(\\w*).x", "\\1", by.approx.x.tot, perl = T )
 
                     d <-
                         d[ , c( setdiff( names( d ), d.n ), d.n ) ] } } }
